f001056017d3afdfe0b132a9a6781399c7f6ff61
Antonio Oro Dungao <antoniodungao@yahoo.com>
2013-10-14 17:16:40 +0300

    Lecture36 Commit

--- src/gui/PrefsDialog.java
+++ src/gui/PrefsDialog.java
@@ -1,6 +1,7 @@
 package gui;
 
 import javax.swing.*;
+import javax.swing.border.Border;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -34,45 +35,8 @@ public class PrefsDialog extends JDialog {
         passField = new JPasswordField(10);
         passField.setEchoChar('*');
 
-        setLayout(new GridBagLayout());
+        layoutControls();
 
-        GridBagConstraints gc = new GridBagConstraints();
-
-        gc.weightx = 1;
-        gc.weighty = 1;
-        gc.fill = GridBagConstraints.NONE;
-
-        /////////////////FIRST ROW////////////////////////////////////
-        gc.gridy = 0;
-        gc.gridx = 0;
-        add(new JLabel("User: "), gc);
-
-        gc.gridx++;
-        add(userField, gc);
-        //////////////////NEXT ROW/////////////////////////////////////
-        gc.gridx = 0;
-        gc.gridy++;
-        add(new JLabel("Password: "), gc);
-        gc.gridx++;
-        add(passField, gc);
-        //////////////////NEXT ROW////////////////////////////////////
-        gc.gridx = 0;
-        gc.gridy++;
-
-        add(new JLabel("Port: "), gc);
-
-        gc.gridx++;
-        add(portSpinner, gc);
-
-        //////////////////NEXT ROW////////////////////////////////////
-        gc.gridx = 0;
-        gc.gridy++;
-
-        add(okButton, gc);
-
-        gc.gridx++;
-        add(cancelButton, gc);
-        ///////////////////////////////////////////////////////////////
         okButton.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
                 Integer port = (Integer)portSpinner.getValue();
@@ -100,6 +64,78 @@ public class PrefsDialog extends JDialog {
         //setResizable(false);
 
 
+    }
+
+    private void layoutControls(){
+
+        JPanel controlPanel = new JPanel();
+        JPanel buttonPanel = new JPanel();
+
+        int space = 15;
+        Border spaceBorder = BorderFactory.createEmptyBorder(space, space, space, space);
+        Border titleBorder = BorderFactory.createTitledBorder("Database Preferences");
+
+        controlPanel.setBorder(BorderFactory.createCompoundBorder(spaceBorder, titleBorder));
+        buttonPanel.setBorder(BorderFactory.createLineBorder(Color.black));
+
+        controlPanel.setLayout(new GridBagLayout());
+        setLayout(new GridBagLayout());
+
+        GridBagConstraints gc = new GridBagConstraints();
+
+        gc.weightx = 1;
+        gc.weighty = 1;
+        gc.fill = GridBagConstraints.NONE;
+        Insets rightpadding = new Insets(0, 0, 0 , 15);
+        Insets noPadding = new Insets(0, 0, 0 , 0);
+
+        /////////////////FIRST ROW////////////////////////////////////
+        gc.gridy = 0;
+        gc.gridx = 0;
+        gc.anchor = GridBagConstraints.EAST;
+        gc.insets = rightpadding;
+        controlPanel.add(new JLabel("User: "), gc);
+
+        gc.insets = noPadding;
+        gc.anchor = GridBagConstraints.WEST;
+        gc.gridx++;
+        controlPanel.add(userField, gc);
+        //////////////////NEXT ROW/////////////////////////////////////
+        gc.gridx = 0;
+        gc.gridy++;
+        gc.anchor = GridBagConstraints.EAST;
+        gc.insets = rightpadding;
+        controlPanel.add(new JLabel("Password: "), gc);
+
+        gc.insets = noPadding;
+        gc.anchor = GridBagConstraints.WEST;
+        gc.gridx++;
+        controlPanel.add(passField, gc);
+        //////////////////NEXT ROW////////////////////////////////////
+        gc.gridx = 0;
+        gc.gridy++;
+        gc.anchor = GridBagConstraints.EAST;
+        gc.insets = rightpadding;
+        controlPanel.add(new JLabel("Port: "), gc);
+
+        gc.insets = noPadding;
+        gc.anchor = GridBagConstraints.WEST;
+        gc.gridx++;
+        controlPanel.add(portSpinner, gc);
+
+        //////////////////Buttons Panel////////////////////////////////////
+        buttonPanel.setLayout(new FlowLayout(FlowLayout.RIGHT));
+
+        buttonPanel.add(okButton);
+        buttonPanel.add(cancelButton);
+
+        Dimension btnSize = cancelButton.getPreferredSize();
+        okButton.setPreferredSize(btnSize);
+
+        /////////////////Add subPanels to the Dialog/////////////////////////
+        setLayout(new BorderLayout());
+        add(controlPanel, BorderLayout.CENTER);
+        add(buttonPanel, BorderLayout.SOUTH);
     }
 
     public void setPreferenceListener(PrefsListener pref){
